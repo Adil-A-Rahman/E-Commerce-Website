1232d3896acd0453aec020583b66685e5f93b2f4
Adil Abdul Rahman <69046477+Adil-A-Rahman@users.noreply.github.com>
2022-01-11 13:58:29 +0530

    token Module

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,6 +1,7 @@
 const ErrorHander = require("../utils/errorhander");
 const catchAsyncErrors = require("../middleware/catchAsyncErrors");
 const User = require("../models/userModel");
+const sendToken = require("../utils/jwtToken")
 
 
 //Register the user
@@ -16,12 +17,7 @@ exports.registerUser = catchAsyncErrors(async (req, res, next)=>{
         }
     });
 
-    const token = user.getJWTToken();
-
-    res.status(201).json({
-        success: true,
-        token
-    });
+    sendToken(user, 201, res);
 });
 
 //Login Users
@@ -46,12 +42,7 @@ exports.loginUser =  catchAsyncErrors(async (req, res, next)=>{
         return next(new ErrorHander("Invalid Email or Password", 401))
     }
 
-    const token = user.getJWTToken();
-
-    res.status(200).json({
-        success: true,
-        token
-    });
+    sendToken(user, 200, res);
 });
 
 //Get all users (Admin)
