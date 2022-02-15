38c8568028e24b3f166768ea99ceb023a6977134
Adil Abdul Rahman <69046477+Adil-A-Rahman@users.noreply.github.com>
2022-02-15 20:31:23 +0530

    Added orderModel, create order route

--- backend/app.js
+++ backend/app.js
@@ -10,14 +10,18 @@ app.use(cookieParser())
 //Routes import
 const product = require("./routes/productRoute");
 const user = require("./routes/userRoute");
+const order = require("./routes/orderRoute");
 
 //Use product routes
-app.use("/api/v1",product);
+app.use("/api/v1", product);
+
 //Use user routes
-app.use("/api/v1",user);
+app.use("/api/v1", user);
+
+//User order route
+app.use("/api/v1", order)
 
 //Middleware for error
 app.use(errorMiddleware);
-
 
 module.exports = app;
