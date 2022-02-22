b520dbc10492b2d8b70dc4ac4a0a9f0d52f38d89
Adil Abdul Rahman <69046477+Adil-A-Rahman@users.noreply.github.com>
2022-02-22 13:11:03 +0530

    Worked on Home page

--- frontend/src/component/Home/Home.js
+++ frontend/src/component/Home/Home.js
@@ -2,6 +2,7 @@ import React, { Fragment } from "react";
 import { CgMouse } from "react-icons/cg";
 import "./Home.css";
 import Product from "./Product.js"
+import MetaData from "../layout/MetaData";
 
 const product = {
     name:"Blue shirt",
@@ -12,6 +13,8 @@ const product = {
 
 const Home = () => {
     return (<Fragment>
+        <MetaData title='Ecommerce Website' />
+
         <div className="banner">
             <p>Welcome to Ecommerce</p>
             <h1>Find amazing products below</h1>
@@ -25,6 +28,13 @@ const Home = () => {
         <h2 className="homeHeading">Featured Products</h2>
 
         <div className="container" id="container">
+            <Product product={product} />
+            <Product product={product} />
+            <Product product={product} />
+            <Product product={product} />
+            <Product product={product} />
+            <Product product={product} />
+            <Product product={product} />
             <Product product={product} />
         </div>
 
